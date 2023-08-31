c03f8700d3edc7ae4172922fe04d9fc36c887776
GoldfishFive <goldfish_42@163.com>
2023-08-31 20:59:39 +0800

    添加pm2运行脚本

--- tools/benchmarks/classification/mim_dist_train.sh
+++ tools/benchmarks/classification/mim_dist_train.sh
@@ -7,10 +7,12 @@ PRETRAIN=$2
 GPUS=${GPUS:-8}
 PY_ARGS=${@:3}
 
+WORK_DIR="$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/$(echo $PRETRAIN | rev | cut -d/ -f 1 | rev)"
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 mim train mmcls $CFG \
     --launcher pytorch -G $GPUS \
+    --work-dir $WORK_DIR \
     --cfg-options model.backbone.init_cfg.type=Pretrained \
     model.backbone.init_cfg.checkpoint=$PRETRAIN \
     model.backbone.init_cfg.prefix="backbone." \
